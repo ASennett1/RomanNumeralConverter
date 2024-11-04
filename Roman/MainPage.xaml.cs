806d5c50e7073bd9692ae5b8d4e4837379d821a6
Andy Sennett <andy.sennett2167@fvtc.edu>
2024-11-03 16:55:37 -0800

    Finished Roman Numeral Converter

---
+++ Roman/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using Roman;
+
+namespace Roman
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Convert(string input)
+        {
+            if (int.TryParse(input, out int number))
+            {
+                return Converter.NumberToRoman(number);
+            }
+            else
+            {
+                return Converter.RomanToNumber(input).ToString();
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Convert_OnClicked(object sender, EventArgs e)
+        {
+            string input = txtPreConvert.Text;
+
+            string result = Convert(input);
+
+            lblPostConvert.Text = result;
+        }
+    }
+}
